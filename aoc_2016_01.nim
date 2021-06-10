817ba26202b966767754bb6bee5f4c4c3a1f1dda
Duncan Clarke <bunkford@gmail.com>
2021-06-10 09:12:58 -0400

    Day 1 Part 2

--- aoc_2016_01.nim
+++ aoc_2016_01.nim
@@ -12,8 +12,7 @@ proc get_input(file:string):seq[string] =
 proc solve1(input: seq[string]):int =
   var coords: array[2, int]
   var d = ["N", "E", "S", "W"]
-  var dir = {"N": [-1, 0], "S": [1, 0], "E": [0, 1], "W": [0, -1]}.toTable
-
+  var dir = {"N": [0, 1], "S": [0, -1], "E": [1, 0], "W": [-1, 0]}.toTable # [x, y]
   var current = 0 # assert d[0] == "N"
 
   for direction in input:
@@ -27,11 +26,10 @@ proc solve1(input: seq[string]):int =
         if current > 0: dec(current)
         else: current = d.high 
     
-    for i in 1 .. steps.parseInt:
-      var x = d[current]
-      var i = dir[x]
-      coords[0] += i[0]
-      coords[1] += i[1]
+    var x = d[current]
+    var i = dir[x]
+    coords[0] += (i[0] * steps.parseInt)
+    coords[1] += (i[1] * steps.parseInt)
   
   #[ 
     Taxicab Distance between (x1, y1) and (x2, y2) = |x2 - x1| + |y2 - y1|
@@ -39,8 +37,43 @@ proc solve1(input: seq[string]):int =
     We started at [0, 0], so all we have to do is add the absolute values of coordinates.
   ]# 
   result = abs(coords[0]) + abs(coords[1])
-        
+
+
+proc solve2(input: seq[string]):int =
+  var coords: array[2, int]
+  var d = ["N", "E", "S", "W"]
+  var dir = {"N": [0, 1], "S": [0, -1], "E": [1, 0], "W": [-1, 0]}.toTable # [x, y]
+  var current = 0 # assert d[0] == "N"
+  var clist: seq[array[2, int]]
+
+  clist.add([0,0])
+  for direction in input:
+    var turn = direction[0 .. 0]
+    var steps = direction[1 .. ^1]
+    case turn:
+      of "R":
+        if current < 3: inc(current)
+        else: current = 0
+      of "L":
+        if current > 0: dec(current)
+        else: current = d.high 
+    
+    var x = d[current]
+    var i = dir[x]
+    for d in 1 .. steps.parseInt:
+      coords[0] += i[0]
+      coords[1] += i[1]
+
+      if coords in clist:
+        return abs(coords[0]) + abs(coords[1])
+
+      clist.add(coords) 
+
+
 var input = get_input(getAppDir() / "aoc_2016_01.txt")
 
 echo "Answer Part 1: ", solve1(input)
+echo "Answer Part 2: ", solve2(input)
+
+
 
