1bee615fbb6ca6f5ff6718753cc121945dc6420e
Duncan Clarke <bunkford@gmail.com>
2021-06-10 22:12:49 -0400

    Day 5 Part 2

--- aoc_2016_05.nim
+++ aoc_2016_05.nim
@@ -1,4 +1,4 @@
-import md5
+import md5, strutils
 
 proc solve1(input: string):string =
   var index:int = 0
@@ -13,9 +13,27 @@ proc solve1(input: string):string =
     inc(index)
     hash = $tomd5(input & $index)
 
+proc solve2(input: string):array[8, char] =
+  var index:int = 0
+  var hash = $tomd5(input & $index)
+
+  while '\x00' in result:
+    while $hash[0..4] != "00000":
+      hash = $tomd5(input & $index)
+      inc(index)
+
+    var i:int
+    if hash[5].isDigit():
+      i = parseInt($hash[5])
+      if i <= result.high and result[i] == '\x00':
+          var digit = hash[6]
+          result[i] = digit
+    
+    inc(index)
+    hash = $tomd5(input & $index)
+
 
 const input = "ffykfhsq"
 
 echo "Answer Part 1: ", solve1(input)
-
-
+echo "Answer Part 2: ", solve2(input).join()
