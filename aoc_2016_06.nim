ebfa79e2b2a8af30958db62f32008c292ab27f47
Duncan Clarke <bunkford@gmail.com>
2021-06-11 08:43:39 -0400

    Removed nested seq that wasn't required

--- aoc_2016_06.nim
+++ aoc_2016_06.nim
@@ -1,22 +1,22 @@
 import os, tables
 
 # put input into seq
-proc get_input(file:string):seq[seq[string]] =
+proc get_input(file:string):seq[string] =
   for line in lines file:
     result.add(@[line])
 
-proc solve1(input:seq[seq[string]]):string =
-  for i in 0 .. input[0][0].high:
+proc solve1(input:seq[string]):string =
+  for i in 0 .. input[0].high:
     var char_count = initCountTable[char]() 
     for row in input:
-      char_count.inc(row[0][i])
+      char_count.inc(row[i])
     result = result & char_count.largest().key
 
-proc solve2(input:seq[seq[string]]):string =
-  for i in 0 .. input[0][0].high:
+proc solve2(input:seq[string]):string =
+  for i in 0 .. input[0].high:
     var char_count = initCountTable[char]() 
     for row in input:
-      char_count.inc(row[0][i])
+      char_count.inc(row[i])
     result = result & char_count.smallest().key
 
 var input = get_input(getAppDir() / "aoc_2016_06.txt")
