86384b775d0047c828496ddc3fab9af320157692
Duncan Clarke <bunkford@gmail.com>
2021-06-16 08:56:10 -0400

    Day 9 Part 2

--- aoc_2016_09.nim
+++ aoc_2016_09.nim
@@ -1,12 +1,11 @@
-import os, strutils
+import os, strutils, sequtils
 
 proc get_input(file:string):string =
   result = readFile file
 
-proc solve1(input:string):int = 
+proc solve1(input:string):string = 
     var decompressed:string
     var marker:string
-    var record:bool = true
     var i:int
     while i <= input.high:
       if input[i] == '(':
@@ -18,8 +17,25 @@ proc solve1(input:string):int =
         decompressed = decompressed & input[i]
         inc(i)
 
-    return decompressed.len
+    return decompressed
+
+proc solve2(input:string):BiggestInt =
+  var weights = newSeqWith(input.len, 1)
+  var marker:string
+  var i:int
+  while i <= input.high:
+    if input[i] == '(':
+      var e = input.find(')', i)
+      marker = input[i + 1 ..  e - 1]
+      for i in e + 1 .. e + marker.split('x')[0].parseInt():
+        weights[i] *= marker.split('x')[1].parseInt()
+      i = e + 1
+    else:
+      inc(result, weights[i])
+      inc(i)
+
 
 var input = get_input(getAppDir() / "aoc_2016_09.txt")
 
-echo "Answer Part 1: ", solve1(input) 
+echo "Answer Part 1: ", solve1(input).len 
+echo "Answer Part 2: ", solve2(input) # low 10116460
