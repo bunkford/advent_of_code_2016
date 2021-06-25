aec7ce601ceb914fbfcec8c19a2990455dc0ec86
Duncan Clarke <bunkford@gmail.com>
2021-06-25 09:22:54 -0400

    Day 15 part 2

--- aoc_2016_15.nim
+++ aoc_2016_15.nim
@@ -6,7 +6,6 @@
     Disc #5 has 7 positions; at time=0, it is at position 2.
     Disc #6 has 19 positions; at time=0, it is at position 17.
 ]#
-
 proc d1(t:int):bool = return (t+11) mod 13 == 0
 proc d2(t:int):bool = return (t+0) mod 5 == 0
 proc d3(t:int):bool = return (t+11) mod 17 == 0
@@ -14,6 +13,8 @@ proc d4(t:int):bool = return (t+0) mod 3 == 0
 proc d5(t:int):bool = return (t+2) mod 7 == 0
 proc d6(t:int):bool = return (t+17) mod 19 == 0
 
+# disc added in part 2
+proc d7(t:int):bool = return (t+0) mod 11 == 0
 
 proc solve1():int = 
   while true:
@@ -21,5 +22,11 @@ proc solve1():int =
           return
       inc(result)
 
+proc solve2():int = 
+  while true:
+      if d1(result+1) and d2(result+2) and d3(result+3) and d4(result+4) and d5(result+5) and d6(result+6) and d7(result+7):
+          return
+      inc(result)
 echo "Answer Part 1: ", solve1()
+echo "Answer Part 2: ", solve2()
 
